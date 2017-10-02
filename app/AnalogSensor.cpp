360563d8a235ab782790d8d3242e716036498a5b
ShaotuJia <shaotu.jia@hotmail.com>
2017-10-02 16:23:58 -0400

    solve two errors and outputs valgrind results in 'outputs' folder

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,7 +1,7 @@
 #include <AnalogSensor.hpp>
 #include <numeric>
 #include <vector>
-
+#include <memory>
 AnalogSensor::AnalogSensor(unsigned int samples)
     : mSamples(samples)
 {
@@ -13,8 +13,9 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
-
+    //std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+    // change the memory-leak code to following:
+    auto readings = std::make_unique<std::vector<int>>(mSamples, 10); 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
 }
